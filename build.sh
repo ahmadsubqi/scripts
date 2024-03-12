d6b510d8573a7fad33527bd3184849975dd72ef3
ahmadsubqi <masmamad97@gmail.com>
2024-03-13 02:32:06 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 #sync
 repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
-git clone https://github.com/sundrams-playground/local_manifests.git -b udc .repo/local_manifests
+git clone https://github.com/ahmadsubqi/local_manifests.git -b udc .repo/local_manifests
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 
 # build
 . build/envsetup.sh
-lunch lineage_m307f-eng
+lunch lineage_m215f-eng
 mka bacon
